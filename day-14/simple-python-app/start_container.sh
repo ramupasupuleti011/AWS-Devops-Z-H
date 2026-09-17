7d5894ae77e5c0226a1065cd29b5cab06f001bd9
Ramu Pasupuleti <126960469+ramupasupuleti011@users.noreply.github.com>
2026-09-17 19:28:57 +0530

    Correct Docker commands in start_container.sh
    Fix syntax errors in start_container.sh for Docker commands.

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,8 +1,12 @@
+
 #!/bin/bash
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker ramupasupuleti11/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 ramupasupuleti11/simple-python-flask-app:latest
+
+
+
